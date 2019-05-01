513d0d6595c1126313137993cac7390183416c5b
s183214 <45406081+s183214@users.noreply.github.com>
2019-05-01 15:40:58 +0200

    add date picker

--- mobile_app/lib/screens/select_date.dart
+++ mobile_app/lib/screens/select_date.dart
@@ -1,7 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:sleep_well/components/scaffold.dart';
 
-class SelectDate extends StatelessWidget {
+class SelectDate extends StatefulWidget {
+  SelectDate({Key key}) : super(key: key);
+
+  _SelectDateState createState() => _SelectDateState();
+}
+
+class _SelectDateState extends State<SelectDate> {
+  DateTime _dateTime;
+  TimeOfDay _timeOfDay = new TimeOfDay.now();
+
+  Future<Null> _selectDate(BuildContext context) async {
+    final DateTime picked = await showDatePicker(
+        context: context,
+        initialDate: _dateTime,
+        firstDate: new DateTime(2016),
+        lastDate: new DateTime(2019));
+    if (picked != null && picked != _dateTime) {
+      print('date selected: ${_dateTime.toString()}');
+      setState(() {
+        _dateTime = picked;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return ScreenScaffold(
@@ -10,7 +33,9 @@ class SelectDate extends StatelessWidget {
         children: <Widget>[
           ListTile(
             title: Text(
-              "Select Date",
+              _dateTime == null
+                  ? "Please select a date"
+                  : "'date selected: ${_dateTime.toString()}'", //Select Date
               style: TextStyle(
                 fontFamily: 'Comfortaa',
                 color: Color(0xFF382E21),
@@ -21,7 +46,14 @@ class SelectDate extends StatelessWidget {
               Icons.calendar_today,
               color: Color(0xFF382E21),
             ),
-          )
+          ),
+          RaisedButton(
+              child: new Text("Select Date"),
+              onPressed: () {
+                Text("text");
+                //_dateTime = new DateTime.now();
+                //_selectDate(context);
+              }),
         ],
       ),
     ));
