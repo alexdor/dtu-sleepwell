dba6846fab84d5594ab733263e8833a6a5274369
Alexandros Dorodoulis <aldor94@gmail.com>
2019-05-08 11:01:35 +0200

    fix: Fix render of input

--- mobile_app/lib/screens/select_date.dart
+++ mobile_app/lib/screens/select_date.dart
@@ -141,30 +141,28 @@ class _SelectDateState extends State<SelectDate> {
                   ),
                 ),
               ),
-              // ListTile(
-              //   leading: FlutterSlider(
-              //     min: 0.0,
-              //     max: 20.0,
-              //     values: [_value],
-              //     handler: FlutterSliderHandler(
-              //         decoration: BoxDecoration(),
-              //         child: Material(
-              //           type: MaterialType.transparency,
-              //           child: Container(
-              //             //padding: EdgeInsets.all(5),
-              //             child: Icon(
-              //               Icons.brightness_1,
-              //               color: Color(0xFFE08E79),
-              //             ),
-              //           ),
-              //         )),
-              //     trackBar: FlutterSliderTrackBar(
-              //       activeTrackBarColor: Color(0xFFE08E79),
-              //       activeTrackBarHeight: 5,
-              //       inactiveTrackBarColor: Colors.grey.withOpacity(0.5),
-              //     ),
-              //   ),
-              // ),
+              FlutterSlider(
+                min: 0.0,
+                max: 20.0,
+                values: [_value],
+                handler: FlutterSliderHandler(
+                    decoration: BoxDecoration(),
+                    child: Material(
+                      type: MaterialType.transparency,
+                      child: Container(
+                        //padding: EdgeInsets.all(5),
+                        child: Icon(
+                          Icons.brightness_1,
+                          color: Color(0xFFE08E79),
+                        ),
+                      ),
+                    )),
+                trackBar: FlutterSliderTrackBar(
+                  activeTrackBarColor: Color(0xFFE08E79),
+                  activeTrackBarHeight: 5,
+                  inactiveTrackBarColor: Colors.grey.withOpacity(0.5),
+                ),
+              ),
               Divider(),
               ListTile(
                 title: Text(
@@ -178,79 +176,73 @@ class _SelectDateState extends State<SelectDate> {
                   ),
                 ),
               ),
-              // ListTile(
-              //   leading: Column(
-              //     children: <Widget>[
-              //       new Row(
-              //         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              //         children: <Widget>[
-              //           RawMaterialButton(
-              //             fillColor: Color(0xFF789CE9),
-              //             splashColor: Colors.orangeAccent,
-              //             child: Padding(
-              //               child: Column(
-              //                 children: <Widget>[
-              //                   SizedBox(
-              //                     width: 100.0,
-              //                   ),
-              //                   Text(
-              //                     'Yes',
-              //                     textAlign: TextAlign.center,
-              //                     style: TextStyle(
-              //                       color: Colors.white,
-              //                       fontFamily: 'Comfortaa',
-              //                       fontSize: 20.0,
-              //                     ),
-              //                   ),
-              //                 ],
-              //               ),
-              //               padding: EdgeInsets.all(10.0),
-              //             ),
-              //             elevation: 3.0,
-              //             onPressed: () {
-              //               //Navigator.push(
-              //               //context,
-              //               //MaterialPageRoute(builder: (context) => SecondIntroPage()),
-              //               //);
-              //             },
-              //             shape: StadiumBorder(),
-              //           ),
-              //           RawMaterialButton(
-              //             fillColor: Color(0xFF789CE9),
-              //             splashColor: Colors.orangeAccent,
-              //             child: Padding(
-              //               child: Column(
-              //                 children: <Widget>[
-              //                   SizedBox(
-              //                     width: 100.0,
-              //                   ),
-              //                   Text(
-              //                     'No',
-              //                     textAlign: TextAlign.center,
-              //                     style: TextStyle(
-              //                       color: Colors.white,
-              //                       fontFamily: 'Comfortaa',
-              //                       fontSize: 20.0,
-              //                     ),
-              //                   ),
-              //                 ],
-              //               ),
-              //               padding: EdgeInsets.all(10.0),
-              //             ),
-              //             elevation: 3.0,
-              //             onPressed: () {
-              //               //Navigator.push(
-              //               //context,
-              //               //MaterialPageRoute(builder: (context) => SecondIntroPage()),
-              //               //);
-              //             },
-              //             shape: StadiumBorder(),
-              //           ),
-              //         ],
-              //       )
-              //     ],
-              //   ),
-              // ),
+              new Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  RawMaterialButton(
+                    fillColor: Color(0xFF789CE9),
+                    splashColor: Colors.orangeAccent,
+                    child: Padding(
+                      child: Column(
+                        children: <Widget>[
+                          SizedBox(
+                            width: 100.0,
+                          ),
+                          Text(
+                            'Yes',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontFamily: 'Comfortaa',
+                              fontSize: 20.0,
+                            ),
+                          ),
+                        ],
+                      ),
+                      padding: EdgeInsets.all(10.0),
+                    ),
+                    elevation: 3.0,
+                    onPressed: () {
+                      //Navigator.push(
+                      //context,
+                      //MaterialPageRoute(builder: (context) => SecondIntroPage()),
+                      //);
+                    },
+                    shape: StadiumBorder(),
+                  ),
+                  RawMaterialButton(
+                    fillColor: Color(0xFF789CE9),
+                    splashColor: Colors.orangeAccent,
+                    child: Padding(
+                      child: Column(
+                        children: <Widget>[
+                          SizedBox(
+                            width: 100.0,
+                          ),
+                          Text(
+                            'No',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontFamily: 'Comfortaa',
+                              fontSize: 20.0,
+                            ),
+                          ),
+                        ],
+                      ),
+                      padding: EdgeInsets.all(10.0),
+                    ),
+                    elevation: 3.0,
+                    onPressed: () {
+                      //Navigator.push(
+                      //context,
+                      //MaterialPageRoute(builder: (context) => SecondIntroPage()),
+                      //);
+                    },
+                    shape: StadiumBorder(),
+                  ),
+                ],
+              ),
               Divider(),
               ListTile(
                 title: TextField(
