1e6c0b705264844dae861065675258a83157af3c
Alexandros Dorodoulis <aldor94@gmail.com>
2019-05-14 19:34:49 +0200

    feat: update esp code

--- esp8266/sleep_well_tracker.ino
+++ esp8266/sleep_well_tracker.ino
@@ -3,20 +3,18 @@
 #include <ESP8266HTTPClient.h>
 #include <ArduinoJson.h>
 #include <WiFiClientSecureBearSSL.h>
+// #include <WiFiClientSecure.h>
 
-#define DHTPIN 2     // what digital pin the DHT22 is conected to
-#define DHTTYPE DHT22   // there are multiple kinds of DHT sensors
+#define DHTPIN 2      // what digital pin the DHT22 is conected to
+#define DHTTYPE DHT22 // there are multiple kinds of DHT sensors
 
-
-const char* WIFI_SSID = "Just passing along";
-const char* WIFI_PASS = "!!!GemistaGiati_misoumeJava!!!";
+const char *WIFI_SSID = "Just passing along";             // WiFi SSID
+const char *WIFI_PASS = "!!!GemistaGiati_misoumeJava!!!"; // WiFi password
 
 DHT dht(DHTPIN, DHTTYPE);
 
-
-
-void wifiConnect() {
-
+void wifiConnect()
+{
   // Connecting to WiFi network
   Serial.println();
   Serial.print("Connecting to ");
@@ -24,7 +22,7 @@ void wifiConnect() {
 
   // ---------------This was the magic WiFi reconnect fix for me https://github.com/esp8266/Arduino/issues/2186
   WiFi.persistent(false);
-  WiFi.mode(WIFI_OFF);   // this is a temporary line, to be removed after SDK update to 1.5.4
+  WiFi.mode(WIFI_OFF); // this is a temporary line, to be removed after SDK update to 1.5.4
   WiFi.mode(WIFI_STA);
   // ---------------END - WiFi reconnect fix
 
@@ -32,9 +30,11 @@ void wifiConnect() {
 
   unsigned long wifiConnectStart = millis();
 
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED)
+  {
     // Check to see if
-    if (WiFi.status() == WL_CONNECT_FAILED) {
+    if (WiFi.status() == WL_CONNECT_FAILED)
+    {
       Serial.println("Failed to connect to WIFI. Please verify credentials: ");
       Serial.println();
       Serial.print("SSID: ");
@@ -47,7 +47,8 @@ void wifiConnect() {
     delay(500);
     Serial.println("...");
     // Only try for 5 seconds.
-    if (millis() - wifiConnectStart > 5000) {
+    if (millis() - wifiConnectStart > 5000)
+    {
       Serial.println("Failed to connect to WiFi");
       Serial.println("Please attempt to send updated configuration parameters.");
       return;
@@ -57,14 +58,15 @@ void wifiConnect() {
   Serial.println("WiFi connected");
 }
 
-
-void setup() {
+void setup()
+{
   Serial.begin(9600);
   Serial.setTimeout(2000);
 
-
   // Wait for serial to initialize.
-  while (!Serial) { }
+  while (!Serial)
+  {
+  }
 
   dht.begin();
 
@@ -73,16 +75,16 @@ void setup() {
   Serial.println("-------------------------------------");
   Serial.println("Running DHT!");
   Serial.println("-------------------------------------");
-
 }
 
-void sendMessage(double humidity, double temp, double heatIndex) {
+void sendMessage(double humidity, double temp, double heatIndex)
+{
   BearSSL::WiFiClientSecure client;
   client.setInsecure();
   HTTPClient https;
 
-  https.begin(client, "quomodo.serveo.ne", 443, "/esp");    //Specify request destination
-  https.addHeader("Content-Type", "text/json");  //Specify content-type header
+  https.begin(client, "sleep-well.cdrproject.com", 80, "/esp");
+  https.addHeader("Content-Type", "text/json"); //Specify content-type header
   StaticJsonDocument<500> jsonBuffer;
 
   jsonBuffer["humidity"] = humidity;
@@ -91,30 +93,31 @@ void sendMessage(double humidity, double temp, double heatIndex) {
 
   String msg;
   serializeJson(jsonBuffer, msg);
-  int httpCode = https.POST(msg);   //Send the request
-  String payload = https.getString();                  //Get the response payload
+  int httpCode = https.POST(msg);     //Send the request
+  String payload = https.getString(); //Get the response payload
 
   Serial.print("http code:");
-  Serial.println(httpCode);   //Print HTTP return code
+  Serial.println(httpCode); //Print HTTP return code
   Serial.print("payload:");
-  Serial.println(payload);    //Print request response payload
+  Serial.println(payload); //Print request response payload
 
-  https.end();  //Close connection
+  https.end(); //Close connection
 }
 
 int timeSinceLastRead = 0;
 
+void loop()
+{
 
-void loop() {
-
-  if (WiFi.status() != WL_CONNECTED) {
+  if (WiFi.status() != WL_CONNECTED)
+  {
     Serial.println("Disconnected from WiFi");
     wifiConnect();
   }
 
-
   // Report every 2 seconds.
-  if (timeSinceLastRead > 2000) {
+  if (timeSinceLastRead > 2000)
+  {
     // Reading temperature or humidity takes about 250 milliseconds!
     // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
     float h = dht.readHumidity();
@@ -122,7 +125,8 @@ void loop() {
     float t = dht.readTemperature();
 
     // Check if any reads failed and exit early (to try again).
-    if (isnan(h) || isnan(t)) {
+    if (isnan(h) || isnan(t))
+    {
       Serial.println("Failed to read from DHT sensor!");
       Serial.printf("h: %d, t: %d \n", isnan(h), isnan(t));
       timeSinceLastRead = 0;
@@ -131,9 +135,6 @@ void loop() {
 
     // Compute heat index in Celsius (isFahreheit = false)
     float hic = dht.computeHeatIndex(t, h, false);
-
-
-
 
     Serial.print("Humidity: ");
     Serial.print(h);
